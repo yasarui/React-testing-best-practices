718e02a2d6dabf2839d1f1afcf48808f371a45e7
Yasar Arafat <ysulaiman@msystechnologies.com>
2022-01-13 20:52:39 +0530

    Added Test cases to the Button Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [background, setBackground] = useState('red');
+  const [disabled, setDisabled] = useState(false)
+  const getButtonText = () => {
+    return background === 'red' ? 'blue' : 'red' 
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <button 
+         disabled={disabled}
+         onClick={()=> background === 'red' ? setBackground('blue') : setBackground('red') } 
+         style={{backgroundColor: disabled ? 'grey' : background}} role="button"> 
+          Change to {getButtonText()}
+       </button>
+       <br/>
+       <input id="disable-button" onChange={()=>setDisabled(!disabled)} type="checkbox" role="checkbox" />
+       <label htmlFor='disable-button'> Disable the button </label>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,77 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { replaceCammelCaseWithSpaces } from './utils';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Color Component test Cases ", ()=>{
+
+  beforeEach(()=> {
+    render(<App />);
+  });
+
+  test('Initial State of a Button', () => {
+
+    const buttonElement = screen.getByRole('button', { name: 'Change to blue' });
+  
+    // Checking weather the button with the Text Exist
+    expect(buttonElement).toBeInTheDocument();
+  
+    //Checking weather button has a background style of red
+    expect(buttonElement).toHaveStyle('backgroundColor: red');
+  
+    //Trigger a click event on the button
+    fireEvent.click(buttonElement);
+  
+    //Check the button color is Changed
+    expect(buttonElement).toHaveStyle('backgroundColor: blue');
+  
+    //Check weather the button text is also changed
+    const blueButton = screen.getAllByRole('button', { name: 'Change to red' })
+  
+  });
+  
+  test('Initial Condition of the Button and the text', () => {
+  
+    //Checking weather the button is in clickable state
+    const buttonElement = screen.getByRole('button', { name: 'Change to blue' });
+    expect(buttonElement).toBeEnabled();
+  
+    //Checkbox is not checked
+    const checkBox = screen.getByRole('checkbox', { name: 'Disable the button' });
+    expect(checkBox).not.toBeChecked();
+  });
+  
+  test('Button is disabled when Checkbox is checked', () => {
+  
+    // Clicking the checkbox
+    const checkBox = screen.getByRole('checkbox', { name: 'Disable the button' });
+    fireEvent.click(checkBox);
+  
+    //Make sure the checkbox is checked
+    expect(checkBox).toBeChecked();
+  
+    // Checking weather the button is disabled
+    const buttonElement = screen.getByRole('button', { name: 'Change to blue' });
+    expect(buttonElement).toBeDisabled();
+    expect(buttonElement).toHaveStyle('backgroundColor: grey');
+  
+    //UnCheck the Checkbox
+    fireEvent.click(checkBox);
+  
+    //Make sure the button is enabled again
+    expect(buttonElement).toBeEnabled();
+  })
+
+});
+
+
+describe('Testing Util functions', () => {
+  test("Works fine for no Capital letters", () => {
+     expect(replaceCammelCaseWithSpaces('Red')).toBe('Red');
+  });
+  test("Works find with one Capital Letters", () => {
+     expect(replaceCammelCaseWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+  });
+  test("Works fine with one or more Captial Letters", () => {
+     expect(replaceCammelCaseWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
+  });
 });
