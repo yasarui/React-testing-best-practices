366a9c09a87f022197d20c79539911bc4d9ed7a0
Yasar Arafat <ysulaiman@msystechnologies.com>
2022-01-16 23:44:27 +0530

    Added the Test Cases

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   const [background, setBackground] = useState('red');
-  const [disabled, setDisabled] = useState(false)
-  const getButtonText = () => {
-    return background === 'red' ? 'blue' : 'red' 
-  }
+  const [disabled, setDisabled] = useState(false);
+  const getButtonText = () => (background === 'red' ? 'blue' : 'red');
   return (
     <div className="App">
-       <button 
-         disabled={disabled}
-         onClick={()=> background === 'red' ? setBackground('blue') : setBackground('red') } 
-         style={{backgroundColor: disabled ? 'grey' : background}} role="button"> 
-          Change to {getButtonText()}
-       </button>
-       <br/>
-       <input id="disable-button" onChange={()=>setDisabled(!disabled)} type="checkbox" role="checkbox" />
-       <label htmlFor='disable-button'> Disable the button </label>
+      <button
+        disabled={disabled}
+        onClick={() => (background === 'red' ? setBackground('blue') : setBackground('red'))}
+        style={{ backgroundColor: disabled ? 'grey' : background }}
+        role="button"
+        type="button"
+      >
+        Change to
+        {' '}
+        {getButtonText()}
+      </button>
+      <br />
+      <label htmlFor="disable-button">
+        Disable the button
+        <input id="disable-button" onChange={() => setDisabled(!disabled)} type="checkbox" role="checkbox" />
+      </label>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -2,76 +2,69 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import { replaceCammelCaseWithSpaces } from './utils';
 import App from './App';
 
-describe("Color Component test Cases ", ()=>{
-
-  beforeEach(()=> {
-    render(<App />);
-  });
-
+describe('Color Component test Cases', () => {
   test('Initial State of a Button', () => {
+    render(<App />);
 
     const buttonElement = screen.getByRole('button', { name: 'Change to blue' });
-  
+
     // Checking weather the button with the Text Exist
     expect(buttonElement).toBeInTheDocument();
-  
-    //Checking weather button has a background style of red
+
+    // Checking weather button has a background style of red
     expect(buttonElement).toHaveStyle('backgroundColor: red');
-  
-    //Trigger a click event on the button
+
+    // Trigger a click event on the button
     fireEvent.click(buttonElement);
-  
-    //Check the button color is Changed
+
+    // Check the button color is Changed
     expect(buttonElement).toHaveStyle('backgroundColor: blue');
-  
-    //Check weather the button text is also changed
-    const blueButton = screen.getAllByRole('button', { name: 'Change to red' })
-  
+
+    // Check weather the button text is also changed
+    screen.getAllByRole('button', { name: 'Change to red' });
   });
-  
+
   test('Initial Condition of the Button and the text', () => {
-  
-    //Checking weather the button is in clickable state
+    render(<App />);
+    // Checking weather the button is in clickable state
     const buttonElement = screen.getByRole('button', { name: 'Change to blue' });
-    expect(buttonElement).toBeEnabled();
-  
-    //Checkbox is not checked
+    expect(buttonElement).toBeDisabled();
+
+    // Checkbox is not checked
     const checkBox = screen.getByRole('checkbox', { name: 'Disable the button' });
     expect(checkBox).not.toBeChecked();
   });
-  
+
   test('Button is disabled when Checkbox is checked', () => {
-  
+    render(<App />);
     // Clicking the checkbox
     const checkBox = screen.getByRole('checkbox', { name: 'Disable the button' });
     fireEvent.click(checkBox);
-  
-    //Make sure the checkbox is checked
+
+    // Make sure the checkbox is checked
     expect(checkBox).toBeChecked();
-  
+
     // Checking weather the button is disabled
     const buttonElement = screen.getByRole('button', { name: 'Change to blue' });
     expect(buttonElement).toBeDisabled();
     expect(buttonElement).toHaveStyle('backgroundColor: grey');
-  
-    //UnCheck the Checkbox
-    fireEvent.click(checkBox);
-  
-    //Make sure the button is enabled again
-    expect(buttonElement).toBeEnabled();
-  })
 
+    // UnCheck the Checkbox
+    fireEvent.click(checkBox);
+
+    // Make sure the button is enabled again
+    expect(buttonElement).toBeEnabled();
+  });
 });
 
-
 describe('Testing Util functions', () => {
-  test("Works fine for no Capital letters", () => {
-     expect(replaceCammelCaseWithSpaces('Red')).toBe('Red');
+  test('Works fine for no Capital letters', () => {
+    expect(replaceCammelCaseWithSpaces('Red')).toBe('Red');
   });
-  test("Works find with one Capital Letters", () => {
-     expect(replaceCammelCaseWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+  test('Works find with one Capital Letters', () => {
+    expect(replaceCammelCaseWithSpaces('MidnightBlue')).toBe('Midnight Blue');
   });
-  test("Works fine with one or more Captial Letters", () => {
-     expect(replaceCammelCaseWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
+  test('Works fine with one or more Captial Letters', () => {
+    expect(replaceCammelCaseWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
   });
 });
